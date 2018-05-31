a654850a657d86d1c55c48b4dd66499d50b7bfe8
chenjiewei123 <2212659654@qq.com>
2018-05-31 11:13:44 +0800

    added

---
+++ MavenDemo/src/main/java/test5_31/MavenDemo/App.java
@@ -0,0 +1,13 @@
+package test5_31.MavenDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ MavenDemo/src/test/java/org/test/test.java
@@ -0,0 +1,14 @@
+package org.test;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;;
+
+
+public class test {
+
+	@Test
+	public void test1() {
+		System.out.println("helloword");
+	}
+
+}
